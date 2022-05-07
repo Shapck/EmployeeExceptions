e8ac6104f3cb8492d4e3ba7f1adf001126449069
Дима <dimonsoldatov@gmsil.com>
2022-05-07 20:39:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/employee/exceptions/proskyemployeeexceptions/ProSkyEmployeeExceptionsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employee.exceptions.proskyemployeeexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSkyEmployeeExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProSkyEmployeeExceptionsApplication.class, args);
+	}
+
+}
